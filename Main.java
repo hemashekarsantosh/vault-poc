7b117cea5220502c5fa03a49f59b4bbe3f0850e8
Santosh Hemashekar <hemashekarsantosh@gmail.com>
2022-12-19 16:29:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Main {
   public static void main(String[] args) {
     // Replace myServer, myUser, myPassword, and myDB with the actual values for your server
-    String connectionUrl = ;
+    String connectionUrl = System.getenv("DB");;
 
     try (Connection connection = DriverManager.getConnection(connectionUrl)) {
       // If the connection was successful, you can now perform various database operations
